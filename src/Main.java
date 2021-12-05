bad8ea3ac24ef9aabb305a334467d5755cd8b383
NikitaMak234 <makarovnikita020395@yandex.ru>
2021-12-05 17:23:17 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {// объявляется класс Test
+
+    public static void main(String[] args) {// так включаются программы,
+        // просто запомнить
+
+        String a = "Hello world";
+        String b = "5 раз";
+
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
